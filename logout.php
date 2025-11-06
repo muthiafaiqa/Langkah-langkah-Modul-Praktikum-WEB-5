1464c7efd668cb86f68c05e5c2c3495219132ea8
muthiafaiqa <mnurfaiqa@gmail.com>
2025-11-06 16:01:41 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php 
+    session_start(); 
+    session_destroy(); 
+ 
+    echo "<script>alert('Anda telah logout')</script>"; 
+    echo "<meta http-equiv='refresh' content='0;url=login.php'>"; 
+?> 
